ab1df3b80d40c388afc2fde7f1046c149787f71c
lizhen325 <zhenli336@gmail.com>
2016-04-27 01:06:27 +1200

    Completed Employee Form validation

--- Web.UI/scripts/Controllers/EmployeeController.js
+++ Web.UI/scripts/Controllers/EmployeeController.js
@@ -14,42 +14,44 @@
                $scope.openDialog = function () {
                    ngDialog.openConfirm({
                        template:
-                           "<form align='center'> " +
+                           "<form align='center' novalidate name='employeeForm' ng-submit='addEmployee(employee)'>" +
                             "<table class='table'>" +
                                 "<h2>Add Employee</h2>" +
                                 "<tr>" +
                                     "<td><label for='name'> Name: </label>" + "</td>" +
-                                    "<td><input type='text' placeholder='Name'/> </td>" +
+                                    "<td><input type='text' name='name' placeholder='Name' ng-model='employee.Name' required/> </td>" +
+                                    "<td style='color:red;' ng-show='employeeForm.name.$error.required && employeeForm.name.$touched'>required</td>" +
                                 "</tr>" +
                                 "<tr>" +
                                     "<td><label for='name'> Phone: </label></td>" +
-                                    "<td><input type='text' placeholder='Phone'/></td>" +
+                                    "<td><input type='text' name='phone' placeholder='Phone' ng-model='employee.Phone' required/></td>" +
+                                    "<td style='color:red;' ng-show='employeeForm.phone.$error.required && employeeForm.phone.$touched'>required</td>" +
                                 "</tr>" +
                                 "<tr>" +
                                     "<td><label for='name'> BirthDay: </label></td>" +
-                                    "<td><input type='text' placeholder='BirthDay'/></td>" +
+                                    "<td><input type='text' placeholder='BirthDay' ng-model='employee.BirthDay'/></td>" +
                                 "</tr>" +
                                 "<tr>" +
                                     "<td><label for='name'> JoinTime: </label></td>" +
-                                    "<td><input type='text' placeholder='JoinTime'/></td>" +
+                                    "<td><input type='text' placeholder='JoinTime' ng-model='employee.JoinTime'/></td>" +
                                 "</tr>" +
                                 "<tr>" +
                                     "<td><label for='name'> SalaryPerHour: </label></td>" +
-                                    "<td><input type='text' placeholder='SalaryPerHour'/></td>" +
+                                    "<td><input type='text' placeholder='SalaryPerHour' ng-model='employee.SalaryPerHour'/></td>" +
                                 "</tr>" +
                                 "<tr>" +
-                                 "<td colspan='2'><input type='submit' value='Add' class='btn btn-primary'/> " +
+                                 "<td colspan='2'><button type='submit' class='btn btn-primary' ng-disabled='employeeForm.$invalid'> Add </button> " +
                                     "<input type='reset' value='reset' class='btn btn-info'/> </td>" +
-                                "</tr>"+
+                                "</tr>" +
                             "</table>" +
                            "</form>",
                        plain: true,
                        scope: $scope
-                   }).then(function (value) {
-                       console.log(value);
-                   }, function (reject) {
-                       console.log(reject);
                    });
+
+                   $scope.addEmployee = function (employee) {
+                       console.log(employee);
+                   };
                };
            };
 })();
