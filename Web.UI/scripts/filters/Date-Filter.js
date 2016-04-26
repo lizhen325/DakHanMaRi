ac2049f4d21796a9b2c864b3365cb5f1f45a7296
lizhen325 <zhenli336@gmail.com>
2016-04-26 20:42:14 +1200

    implementation Date Filter

---
+++ Web.UI/scripts/filters/Date-Filter.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../Venders/angular.js" />
+(function () {
+    'use restrict';
+    angular.module('DakHanMaRi')
+            .filter('formatDateTime', function ($filter) {
+                return function (date) {
+                    return $filter('date')(parseInt(date.substr(6)), 'yyyy-MM-dd');
+                }
+            })
+})();
