8fc961ea5cb304d3490a4959c85b0bcbac909869
lizhen325 <zhenli336@gmail.com>
2016-04-29 23:30:34 +1200

    Show All Products

--- Web.UI/scripts/Controllers/CategoryController.js
+++ Web.UI/scripts/Controllers/CategoryController.js
@@ -9,7 +9,6 @@
             method: 'GET',
             url: '/Category/GetProductCategory',
         }).then(function (response) {
-            console.log(response);
             $scope.productCategories = response.data;
         })
 
@@ -19,7 +18,22 @@
                 url: '/Product/GetProductsByCategoryId',
                 params: { CategoryId: JSON.stringify(categoryId) }
             }).then(function (response) {
-                console.log(response);
+                $scope.products = response.data;
+                ngDialog.openConfirm({
+                    template:
+                            "<div class='row'>" +
+                                "<div class='col-sm-2'>" +
+                                    "<a href='#'><span class='glyphicon glyphicon-list' sytle='border:none;'><strong>Products</strong></span></a>" +
+                                "</div>" +                             
+                            "</div>" +
+                            "<div class='row'>"+
+                                "<div class='col-sm-4'>" +
+                                    "<strong ng-repeat='product in products' style='color:blue; padding:10px;'>{{product.ProductName}}</strong>" +
+                                "</div>"+
+                            "</div>",
+                    plain: true,
+                    scope: $scope
+                });
             })
         }
     }
