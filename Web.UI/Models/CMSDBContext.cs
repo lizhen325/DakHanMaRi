4648c8bb6c6683258480949041ddd13354c9b97b
lizhen325 <zhenli336@gmail.com>
2016-04-29 15:49:42 +1200

    Add Three Tables: Items, Products and Categories. The relationship is one product has some Items; one category has some products. Therefore one to many relathionship items to products, as well as products to categories

--- Web.UI/Models/CMSDBContext.cs
+++ Web.UI/Models/CMSDBContext.cs
@@ -8,12 +8,18 @@ namespace Web.UI.Models
 
         public virtual IDbSet<Employee> Employees { get; set; }
         public virtual IDbSet<AdminProfile> AdminProfiles { get; set; }
+        public virtual IDbSet<Item> Items { get; set; }
+        public virtual IDbSet<Product> Products { get; set; }
+        public virtual IDbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Employee>().ToTable("Employees");
             modelBuilder.Entity<AdminProfile>().ToTable("AdminProfiles");
+            modelBuilder.Entity<Item>().ToTable("Items");
+            modelBuilder.Entity<Product>().ToTable("Products");
+            modelBuilder.Entity<Category>().ToTable("Categories");
         }
     }
 }
---
+++ Web.UI/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web.UI.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemQuality { get; set; }
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
